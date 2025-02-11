b4c37b78ac35e6038807d1227f31289735cbe9be
HerisonMaciel <herisson2maciel@hotmail.com>
2025-02-11 03:20:58 -0300

    add componet 2

---
+++ componente2/src/main/java/com/herison/componente2/Componente2Application.java
@@ -0,0 +1,13 @@
+package com.herison.componente2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Componente2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Componente2Application.class, args);
+	}
+
+}
