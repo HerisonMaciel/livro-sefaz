b3e94c6fb3adbe9c5c334dee418b5e6025837bf0
HerisonMaciel <herisson2maciel@hotmail.com>
2025-02-11 00:24:27 -0300

    add entity

--- componente1/src/main/java/com/herison/componente1/entity/Autor.java
+++ componente1/src/main/java/com/herison/componente1/entity/Autor.java
@@ -1,4 +1,38 @@
 package com.herison.componente1.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "autores")
 public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Livro> livros = new ArrayList<>();
+
+    public void addLivro(Livro livro) {
+        livros.add(livro);
+        livro.setAutor(this);
+    }
+
+    public void removeLivro(Livro livro) {
+        livros.remove(livro);
+        livro.setAutor(null);
+    }
+
 }
--- componente1/src/main/java/com/herison/componente1/entity/Livro.java
+++ componente1/src/main/java/com/herison/componente1/entity/Livro.java
@@ -1,7 +1,32 @@
 package com.herison.componente1.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "livros")
 public class Livro {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 200)
+    private String titulo;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String descricao;
+
+    @Column(nullable = false, length = 50)
+    private String categoria;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "autor_id", nullable = false)
+    private Autor autor;
 
 }
