4f61a1e2a2803e51a7bd2d2676ff4601b2c23911
HerisonMaciel <herisson2maciel@hotmail.com>
2025-02-11 00:44:23 -0300

    add service

--- componente1/src/main/java/com/herison/componente1/service/AutorService.java
+++ componente1/src/main/java/com/herison/componente1/service/AutorService.java
@@ -1,8 +1,14 @@
 package com.herison.componente1.service;
 
+import com.herison.componente1.Dtos.AutorDTO;
+import com.herison.componente1.entity.Autor;
+import com.herison.componente1.mapper.AutorMapper;
 import com.herison.componente1.repository.AutorRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
@@ -10,5 +16,19 @@ public class AutorService {
 
     private AutorRepository autorRepository;
 
+    private AutorMapper autorMapper;
+
+    public AutorDTO criarAutor(AutorDTO autorDTO) {
+        Autor autor = autorMapper.toEntity(autorDTO);
+        Autor savedAutor = autorRepository.save(autor);
+        return autorMapper.toDTO(savedAutor);
+    }
+
+    public List<AutorDTO> listarAutores() {
+        List<Autor> autores = autorRepository.findAll();
+        return autores.stream()
+                .map(autorMapper::toDTO)
+                .collect(Collectors.toList());
+    }
 
 }
---
+++ componente1/src/main/java/com/herison/componente1/service/LivroService.java
@@ -0,0 +1,36 @@
+package com.herison.componente1.service;
+
+import com.herison.componente1.Dtos.LivroDTO;
+import com.herison.componente1.entity.Livro;
+import com.herison.componente1.mapper.LivroMapper;
+import com.herison.componente1.repository.LivroRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@Data
+@Service
+public class LivroService {
+
+    private LivroRepository livroRepository;
+
+    private LivroMapper livroMapper;
+
+    public LivroDTO criarLivro(LivroDTO livroDTO) {
+        Livro livro = livroMapper.toEntity(livroDTO);
+        Livro savedLivro = livroRepository.save(livro);
+        return livroMapper.toDTO(savedLivro);
+    }
+
+    public List<LivroDTO> listarLivros() {
+        List<Livro> livros = livroRepository.findAll();
+        return livros.stream()
+                .map(livroMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+}
